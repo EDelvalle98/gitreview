46b7781c82158d84e521cc3bc71f319dffc3f0e3
EDelvalle98 <efrain.delvalle@hudsoncatholic.org>
2024-09-18 00:22:59 -0400

    feature branch message added

--- Simple.java
+++ Simple.java
@@ -3,5 +3,6 @@ package printjob;
 public class Simple {
     public static void main(String[] args) {
         System.out.println("Hello main or master branch");
+        System.out.println("Hello from the feature branch");
     }
 }
